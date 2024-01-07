4eef15635e2c4f3b92ed309d230970d1f3e18236
stepan.korobeinikov <stepan.korobeinikov@tvbet.tv>
2024-01-07 10:03:33 +0100

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,24 @@
 <script setup lang="ts">
+import { computed, onMounted } from 'vue'
+import useStore from '@/stores'
+
+const store = useStore()
+
+const isWeatherDataValid = computed(() => {
+  return (
+    store.weatherData && 'location' in store.weatherData && 'current' in store.weatherData && 'forecast' in store.weatherData
+  )
+})
+
+onMounted(async () => {
+  store.setDeviceType()
+  await store.setGeolocation()
+  await store.setWeatherData()
+})
 </script>
 
 <template>
-  <div></div>
+  <router-view v-if="isWeatherDataValid"></router-view>
 </template>
 
-<style>
-</style>
+<style lang="scss"></style>
