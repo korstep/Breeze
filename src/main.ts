8fe4e02c37888d942c4cb8e06fdee3c22ea5778d
stepan.korobeinikov <stepan.korobeinikov@tvbet.tv>
2024-01-07 10:03:26 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,13 @@
-import { createApp } from 'vue';
-import { createPinia } from 'pinia';
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 
-import App from './App.vue';
-import router from './router';
+import App from './App.vue'
+import router from './router'
+import '@/assets/styles/base/main.scss'
+import '@/assets/styles/base/variables.css'
 
-const app = createApp(App);
+const app = createApp(App)
 
-app.use(createPinia());
-app.use(router);
-
-app.mount('#app');
+app.use(createPinia())
+app.use(router)
+app.mount('#app')
