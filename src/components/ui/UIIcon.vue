02294a5792f24adafedc75d6884c15ecde71ca55
stepan.korobeinikov <stepan.korobeinikov@tvbet.tv>
2024-01-07 09:47:25 +0100

    Implement svg loader

---
+++ src/components/ui/UIIcon.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+import icons from '@/helpers/icons'
+
+const { name } = defineProps({
+  name: {
+    type: String,
+    required: true
+  },
+  isActive: {
+    type: Boolean,
+    required: false,
+    default: false
+  }
+})
+const iconComponent = computed(() => icons[name])
+</script>
+
+<template>
+  <Component :is="iconComponent" class="icon" />
+</template>
