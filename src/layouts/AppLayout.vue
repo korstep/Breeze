c87437f854fe1de116caf532de40dcc49a65783a
stepan.korobeinikov <stepan.korobeinikov@tvbet.tv>
2024-01-07 10:00:41 +0100

    Create AppLayout.vue

---
+++ src/layouts/AppLayout.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import TheNavigation from '@/components/blocks/TheNavigation.vue'
+import useStore from '@/stores'
+
+const store = useStore()
+</script>
+
+<template>
+  <div class="app-layout">
+    <Teleport to="#app" :disabled="store.isDesktop">
+      <TheNavigation />
+    </Teleport>
+    <router-view></router-view>
+  </div>
+</template>
+
+<style lang="scss">
+@import '@/assets/styles/ui/mixins';
+
+.app-layout {
+  height: calc(100% - 2.5vmax * 2);
+  padding: 2.5vmax;
+
+  @include media('s', 'm') {
+    overflow-y: auto;
+  }
+}
+</style>
