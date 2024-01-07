c57505a484f94c3fdb365540b48af9833639f065
stepan.korobeinikov <stepan.korobeinikov@tvbet.tv>
2024-01-07 10:02:58 +0100

    Create TodaysForecast.vue

---
+++ src/components/blocks/TodaysForecast.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import UISection from '@/components/ui/UISection.vue'
+</script>
+
+<template>
+  <UISection>
+    <div class="todays-forecast">
+      <div class="todays-forecast__body">
+        <small class="todays-forecast__title">today's forecast</small>
+      </div>
+    </div>
+  </UISection>
+</template>
+
+<style scoped lang="scss"></style>
