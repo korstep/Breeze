332cd1264f2040a599174b8c5e88264b6a129276
stepan.korobeinikov <stepan.korobeinikov@tvbet.tv>
2024-01-07 10:01:50 +0100

    Create router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,15 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHistory } from 'vue-router'
+import AppLayout from '@/layouts/AppLayout.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
-});
+  routes: [
+    {
+      path: '/',
+      component: AppLayout,
+      children: [{ path: '', component: () => import('@/pages/Main.vue') }]
+    }
+  ]
+})
 
-export default router;
+export default router
