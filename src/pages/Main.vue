56383b76bf795fdb8727c6bd6a25e7114dd992ae
stepan.korobeinikov <stepan.korobeinikov@tvbet.tv>
2024-01-07 10:03:21 +0100

    Create main page

---
+++ src/pages/Main.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import TheWeather from '@/components/blocks/TheWeather.vue'
+import TodaysForecast from '@/components/blocks/TodaysForecast.vue'
+import NextDaysForecast from '@/components/blocks/NextDaysForecast.vue'
+</script>
+
+<template>
+  <main class="main">
+    <TheWeather />
+    <TodaysForecast />
+    <NextDaysForecast />
+  </main>
+</template>
+
+<style scoped lang="scss">
+.main {
+  display: flex;
+  flex-direction: column;
+}
+</style>
