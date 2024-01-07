fbdb8be06a823a9be17eaa1506dcdec22168c5c1
stepan.korobeinikov <stepan.korobeinikov@tvbet.tv>
2024-01-07 10:01:25 +0100

    Create UISection.vue

---
+++ src/components/ui/UISection.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts"></script>
+
+<template>
+  <section class="section">
+    <div class="section__body">
+      <slot />
+    </div>
+  </section>
+</template>
+
+<style scoped lang="scss">
+section.section {
+  background-color: var(--background-second);
+  border-radius: 22px;
+
+  &__body {
+    padding: 25px;
+  }
+}
+</style>
