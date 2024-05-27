225839a743b892a0193503dd777965573a2959b0
stepan.korobeinikov <stepan.korobeinikov@tvbet.tv>
2024-05-27 20:38:36 +0200

    trash commit 4

--- env.d.ts
+++ env.d.ts
@@ -1,6 +1,7 @@
 /// <reference types="vite/client" />
 interface ImportMetaEnv {
   readonly VITE_WEATHER_API_KEY: string
+  readonly VITE_IPINFO_API_KEY: string
 }
 
 interface ImportMeta {
--- src/layouts/AppLayout.vue
+++ src/layouts/AppLayout.vue
@@ -1,8 +1,12 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import UICaption from '@/shared/components/UICaption.vue'
+import sizes from '@/shared/constants/sizes'
+</script>
 
 <template>
   <div class="app-layout">
     <router-view></router-view>
+    <UICaption class="app-layout__author" :size="sizes.S">Developed by Stepan Korobeinikov</UICaption>
   </div>
 </template>
 
@@ -17,6 +21,14 @@
     height: calc(100dvh - var(--app-layout-padding) * 2);
 
     --app-layout-padding: 1.75dvw;
+  }
+
+  &__author {
+    position: absolute;
+    right: 50%;
+    bottom: calc(var(--app-layout-padding) * 0.1);
+    text-transform: unset;
+    transform: translateX(50%);
   }
 }
 </style>
